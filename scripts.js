d6da40a23ac1f91e1814eb8bb03a9e7a5f21bd97
reixxe <m4ty1111@gmail.com>
2023-01-26 11:31:11 +0100

    update

--- scripts.js
+++ scripts.js
@@ -63,3 +63,17 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  let navbar = document.getElementsByClassName("navbar")[0];
+  if (prevScrollpos > currentScrollPos) {
+    navbar.classList.remove("navbar-hidden");
+    navbar.classList.add("navbar-show");
+  } else {
+    navbar.classList.remove("navbar-show");
+    navbar.classList.add("navbar-hidden");
+  }
+  prevScrollpos = currentScrollPos;
+};
