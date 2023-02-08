8b050d30eb882d4246c227a802d2ceec210d3358
Matěj Tobiáš Moravec <m4ty1111@gmail.com>
2023-02-08 08:29:50 +0100

    alpha 0.2

--- js/lenis-scroll.js
+++ js/lenis-scroll.js
@@ -1,16 +1,21 @@
-const lenis = new Lenis({
-  duration: 0.2,
-  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // penis function (???)
-  direction: "vertical",
-  gestureDirection: "vertical",
-  smooth: true,
-  smoothTouch: false,
-  touchMultiplier: 2,
-});
+// if (
+//   navigator.userAgent.indexOf("Firefox") > -1 ||
+//   navigator.userAgent.indexOf("Mozilla") > -1
+// ) {
+//   const lenis = new Lenis({
+//     duration: 0.2,
+//     easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+//     direction: "vertical",
+//     gestureDirection: "vertical",
+//     smooth: true,
+//     smoothTouch: false,
+//     touchMultiplier: 2,
+//   });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+//   function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+//   }
 
-requestAnimationFrame(raf);
+//   requestAnimationFrame(raf);
+// }
